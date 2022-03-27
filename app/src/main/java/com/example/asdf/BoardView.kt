3b39b6bf5f9ca5afe15849d989d7cf10dd61fc5b
aterr4 <krzus7@gmail.com>
2022-03-28 00:03:05 +0200

    Adjusted grid, working start button

--- app/src/main/java/com/example/asdf/BoardView.kt
+++ app/src/main/java/com/example/asdf/BoardView.kt
@@ -8,15 +8,15 @@ import android.view.View
 
 
 class BoardView(context: Context?, attrs: AttributeSet?): View(context, attrs) {
-    val cellSide: Float = 130f
+    val cellSide: Float = 190f
     var res: Resources = resources
     var bitmap = BitmapFactory.decodeResource(res, R.drawable.tile_empty_light)
-    var bitmaps = Bitmap.createScaledBitmap(bitmap,135,135,false)
+    var bitmaps = Bitmap.createScaledBitmap(bitmap,cellSide.toInt(),cellSide.toInt(),false)
 
     override fun onDraw(canvas: Canvas?) {
         for (row in 0 until 6) {
             for (col in 0 until 5) {
-                canvas?.drawBitmap(bitmaps,150f + (col * cellSide),150f + (row * cellSide),Paint())
+                canvas?.drawBitmap(bitmaps,0f + (col * cellSide),0f + (row * cellSide),Paint())
             }
         }
     }
--- app/src/main/java/com/example/asdf/MainActivity.kt
+++ app/src/main/java/com/example/asdf/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.asdf
 
+import android.content.Intent
 import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
+
+
+        val start_BTN = findViewById(R.id.button10) as Button
+        start_BTN.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
 }
+
