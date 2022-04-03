13fa9ed36e41ddb2645e1cc5e2f961c5382727c6
Wojtek Kopanski <wilq.xp@gmail.com>
2022-04-03 20:28:07 +0200

    move movement to inner class of gameactivity

--- app/src/main/java/com/example/asdf/GameActivity.kt
+++ app/src/main/java/com/example/asdf/GameActivity.kt
@@ -11,19 +11,21 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.GestureDetectorCompat
 import kotlin.math.log
-
+//
 private lateinit var detector: GestureDetectorCompat
-private lateinit var movement: Movement
+//private lateinit var movement: Movement
 
 class GameActivity : AppCompatActivity() {
+    var movement: Movement = Movement(Board)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         detector = GestureDetectorCompat(this, SwipeDetect())
         Board.size = Pair(5,6)
         Board.startingPoint = Pair(0,0)
-        movement = Movement(Board)
-        movement.view = findViewById(R.id.board)
+//        var movement = Movement(Board)
+//        movement.view = findViewById(R.id.board)
+
 
     }
 
@@ -46,6 +48,100 @@ class GameActivity : AppCompatActivity() {
             true
         } else {
             super.onTouchEvent(event)
+        }
+    }
+
+    enum class Direction {
+        LEFT, UP, RIGHT, DOWN
+    }
+
+    inner class Movement(var board: Board) {
+        private var currentPosition: Pair<Int, Int> = board.startingPoint
+        var madeMistake = false
+        var view : View? = null
+        var previousDirection: Direction? = null
+        init {
+            setTile()
+        }
+
+
+        private fun getNextDirection(): Direction? {
+            if (currentPosition.first > 0 && board.isOnTrack(Pair(currentPosition.second, currentPosition.first-1)))
+                return Direction.LEFT
+            if (currentPosition.first < board.size.first-1 && board.isOnTrack(Pair(currentPosition.second, currentPosition.first+1)))
+                return Direction.RIGHT
+            if (currentPosition.second > 0 && board.isOnTrack(Pair(currentPosition.second-1, currentPosition.first)))
+                return Direction.UP
+            if (currentPosition.second < board.size.second-1 && board.isOnTrack(Pair(currentPosition.second+1, currentPosition.first)))
+                return Direction.DOWN
+            return null // it was the last tile
+        }
+
+        // set tile on current position
+        private fun setTile() {
+            if (madeMistake) {
+                when(previousDirection) {
+                    Direction.LEFT -> board.tileBoard[currentPosition.second][currentPosition.first] = Tile.LEFT_YELLOW
+                    Direction.UP -> board.tileBoard[currentPosition.second][currentPosition.first] = Tile.UP_YELLOW
+                    Direction.RIGHT -> board.tileBoard[currentPosition.second][currentPosition.first] = Tile.RIGHT_YELLOW
+                    Direction.DOWN -> board.tileBoard[currentPosition.second][currentPosition.first] = Tile.DOWN_YELLOW
+                }
+            } else {
+                when(getNextDirection()) {
+                    Direction.LEFT -> board.tileBoard[currentPosition.second][currentPosition.first] = Tile.LEFT_GREEN
+                    Direction.UP -> board.tileBoard[currentPosition.second][currentPosition.first] = Tile.UP_GREEN
+                    Direction.RIGHT -> board.tileBoard[currentPosition.second][currentPosition.first] = Tile.RIGHT_GREEN
+                    Direction.DOWN -> board.tileBoard[currentPosition.second][currentPosition.first] = Tile.DOWN_GREEN
+                }
+            }
+        }
+
+        private fun getNextPosition(): Pair<Int, Int> {
+            when (getNextDirection()) {
+                Direction.LEFT -> return Pair(currentPosition.first-1, currentPosition.second)
+                Direction.UP -> return Pair(currentPosition.first, currentPosition.second-1)
+                Direction.RIGHT -> return Pair(currentPosition.first+1, currentPosition.second)
+                Direction.DOWN -> return Pair(currentPosition.first, currentPosition.second+1)
+            }
+            return currentPosition // it was the last tile
+        }
+
+        // on move
+        fun move(direction: Direction) {
+            if (direction == getNextDirection()) {
+                madeMistake = false
+                setTile()
+                // set tile on track to false after completing
+                board.board[currentPosition.second][currentPosition.first] = false
+                currentPosition = getNextPosition()
+                previousDirection = null
+            } else {
+                if (madeMistake) gameOver()
+                madeMistake = true
+                when (direction) {
+                    Direction.LEFT -> {
+                        if (currentPosition.first <= 0) gameOver()
+                        previousDirection = Direction.RIGHT
+                    }
+                    Direction.UP -> {
+                        if (currentPosition.second <= 0) gameOver()
+                        previousDirection = Direction.DOWN
+                    }
+                    Direction.RIGHT -> {
+                        if (currentPosition.first <= board.size.first) gameOver()
+                        previousDirection = Direction.LEFT
+                    }
+                    Direction.DOWN -> {
+                        if (currentPosition.second <= board.size.second) gameOver()
+                        previousDirection = Direction.UP
+                    }
+                }
+                setTile()
+            }
+        }
+
+        fun gameOver() {
+            dialog(findViewById(R.id.board))
         }
     }
 
@@ -93,22 +189,22 @@ class GameActivity : AppCompatActivity() {
     val viewBoard : View = findViewById(R.id.board)
     private fun onSwipeDown(){
         viewBoard.invalidate()
-        movement.move(Movement.Direction.DOWN)
+        movement.move(Direction.DOWN)
     }
 
     private fun onSwipeUp(){
         viewBoard.invalidate()
-        movement.move(Movement.Direction.UP)
+        movement.move(Direction.UP)
     }
 
     internal fun onSwipeLeft(){
         viewBoard.invalidate()
-        movement.move(Movement.Direction.LEFT)
+        movement.move(Direction.LEFT)
     }
 
     internal fun onSwipeRight(){
         viewBoard.invalidate()
-        movement.move(Movement.Direction.RIGHT)
+        movement.move(Direction.RIGHT)
     }
 
 
